4a7bb6bbf4db4812a15d6488f35115b0c7734a2f
H1MSK <ksda47832338@outlook.com>
2023-07-22 16:39:46 +0800

    tweak: add frequency & sim config

--- hw/spinal/cnnsys/ProjectConfig.scala
+++ hw/spinal/cnnsys/ProjectConfig.scala
@@ -3,14 +3,22 @@ package cnnsys
 import spinal.core._
 import spinal.core.sim._
 
+import scala.language.postfixOps
+
 object ProjectConfig {
   def spinal = SpinalConfig(
     targetDirectory = "hw/gen",
     defaultConfigForClockDomains = ClockDomainConfig(
       resetActiveLevel = HIGH
     ),
+    defaultClockDomainFrequency = FixedFrequency(200 MHz),
+//    dumpWave = DumpWaveConfig(depth = 1000),
     onlyStdLogicVectorAtTopLevelIo = true
   )
 
-  def sim = SimConfig.withConfig(spinal).withFstWave
+  def sim = SimConfig
+    .withConfig(spinal)
+    .withVcdWave
+    .workspacePath("sim_out")
+    .allOptimisation
 }
