c21d3da2fd4778c8be4c185170df56e47d0e01fe
H1MSK <ksda47832338@outlook.com>
2023-07-25 12:30:53 +0800

    refactor: requantizer
    fix: round algorithm
    fix: useOffsetSaturation

--- hw/spinal/lib/quantizer/Requantizer.scala
+++ hw/spinal/lib/quantizer/Requantizer.scala
@@ -1,5 +1,6 @@
 package lib.quantizer
 
+import lib.StreamController.StreamController
 import spinal.core._
 import spinal.lib._
 
@@ -14,45 +15,65 @@ case class Requantizer(
   val dout = master Stream Vec(SInt(config.dout_bitwidth bits), parallel_count)
   val param = in(RequantizerParamBundle(config))
 
-  private val din_stage = din.stage()
+  private var last_payload = din.payload
 
-  private var last_stage = din_stage
+  val stage_count = (if (config.scale_bitwidth > 0) 1 else 0) +
+    (if (config.useOffset) 1 else 0) +
+    (if (config.useRightShift) 1 else 0) +
+    (if (
+       config.din_bitwidth +
+         Math.max(0, config.scale_bitwidth) +
+         (if (config.useOffset && !config.useOffsetSaturation) 1 else 0) -
+         config.dout_bitwidth != 0
+     ) 1
+     else 0)
+
+  val controller = StreamController(stage_count)
+  controller << din
+  var current_stage = 0
 
   if (config.scale_bitwidth > 0) {
-    val scale_stage = Stream(Vec(SInt(config.din_bitwidth + config.scale_bitwidth bits), parallel_count))
-    scale_stage.arbitrationFrom(last_stage)
-    scale_stage.payload := Vec(last_stage.payload.map(_ * param.scale))
-    scale_stage.setName("scale_stage")
-    last_stage = scale_stage.stage()
+    val scaled = Vec(last_payload.map(_ * param.scale))
+    last_payload = RegNextWhen(scaled, controller.en(current_stage), init = scaled.getZero)
+    current_stage += 1
+    scaled.setName("scaled")
+    last_payload.setName("scale_stage")
   }
 
   if (config.useOffset) {
-    val offset_stage = Stream(
-      Vec(SInt(config.din_bitwidth + config.scale_bitwidth + (if (config.useOffsetSaturation) 0 else 1) bits), parallel_count)
+    val offset = Vec(
+      last_payload.map(x => if (config.useOffsetSaturation) x +| param.offset else x +^ param.offset)
     )
-    offset_stage.arbitrationFrom(last_stage)
-    offset_stage.payload := Vec(
-      last_stage.payload.map(x => if (config.useOffsetSaturation) x +^ param.offset else x +| param.offset)
-    )
-    offset_stage.setName("offset_stage")
-    last_stage = offset_stage.stage()
+    last_payload = RegNextWhen(offset, controller.en(current_stage), init = offset.getZero)
+    current_stage += 1
+    offset.setName("offset")
+    last_payload.setName("offset_stage")
   }
 
   if (config.useRightShift) {
-    val shift_stage = Stream(Vec(SInt(last_stage.payload.head.getBitsWidth bits), parallel_count))
-    shift_stage.arbitrationFrom(last_stage)
-    shift_stage.payload := Vec(last_stage.payload.map(_ >> param.shift_count))
-    shift_stage.setName("shift_stage")
-    last_stage = shift_stage.stage()
+    val shifted = Vec(last_payload.map(_ >> param.shift_count))
+    last_payload = RegNextWhen(shifted, controller.en(current_stage), init = shifted.getZero)
+    current_stage += 1
+    shifted.setName("shifted")
+    last_payload.setName("shift_stage")
   }
 
-  val round_stage = Stream(Vec(SInt(config.dout_bitwidth bits), parallel_count))
-  round_stage.arbitrationFrom(last_stage)
-  round_stage.payload := Vec(
-    last_stage.payload.map(_.roundToInf(last_stage.payload.head.getBitsWidth - config.dout_bitwidth bits, align = true))
-  )
-  last_stage = round_stage
+  if (last_payload.head.getBitsWidth - config.dout_bitwidth != 0) {
+    val rounded =
+      (if (last_payload.head.getBitsWidth - config.dout_bitwidth > 0)
+         Vec(
+           last_payload.map(_.roundToInf(last_payload.head.getBitsWidth - config.dout_bitwidth bits, align = false).sat(1))
+         )
+       else
+         Vec(
+           last_payload.map(_.resize(config.dout_bitwidth))
+         ))
+    last_payload = RegNextWhen(rounded, controller.en(current_stage), init = rounded.getZero)
+    current_stage += 1
+    rounded.setName("rounded")
+    last_payload.setName("round_stage")
+  }
 
-  dout.arbitrationFrom(last_stage)
-  dout.payload := last_stage.payload
+  controller >> dout
+  dout.payload := last_payload
 }
