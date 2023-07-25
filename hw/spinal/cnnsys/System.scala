53d0ab8ba8445f219b1d6541006caa2a6a0a2c45
H1MSK <ksda47832338@outlook.com>
2023-07-25 12:31:56 +0800

    impl: simple top module

--- hw/spinal/cnnsys/System.scala
+++ hw/spinal/cnnsys/System.scala
@@ -1,22 +1,41 @@
 package cnnsys
 
-import cnnsys.conv_unit.{ConvUnitConfig, ConvCore}
-import spinal.core.Component
-import spinal.lib.bus.amba4.axi.{Axi4, Axi4Config}
-import spinal.lib.bus.amba4.axilite.{AxiLite4, AxiLite4Config}
-import spinal.lib.slave
+import cnnsys.conv_unit.{ConvCore, ConvUnitConfig}
+import lib.quantizer.RequantizerParamBundle
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.amba4.axis.{Axi4Stream, Axi4StreamConfig}
 
-case class System () extends Component {
+import scala.language.postfixOps
+
+case class System() extends Component {
   val config = ConvUnitConfig()
 
-  val io_ctrl = slave(AxiLite4(AxiLite4Config(dataWidth = 32, addressWidth = 16)))
+  val core = ConvCore(config)
 
-}
+  val line_width_sel = in Bits (log2Up(config.supportedInputWidths.length) bits)
 
-object GenVerilog extends App {
-  ProjectConfig.spinal.generateVerilog(ConvCore(ConvUnitConfig())).printPruned()
-}
+  def genStreamPort(bitCount: BitCount) = Axi4Stream(Axi4StreamConfig((bitCount.value + 7) / 8))
 
-object GenVhdl extends App {
-  ProjectConfig.spinal.generateVhdl(ConvCore(ConvUnitConfig())).printPruned()
+  val din_stream = slave(genStreamPort(config.unitInDataBitWidth * config.coreInChannelCount bits))
+  val kernel_data_stream = slave(genStreamPort(config.unitKernelDataBitWidth * config.coreInChannelCount bits))
+  val requantizer_param_stream = slave(genStreamPort(RequantizerParamBundle(config.requantizer_config).getBitsWidth bits))
+  val bias_data = slave(genStreamPort(config.biasDataBitWidth * config.coreOutChannelCount bits))
+  val dout = master(genStreamPort(config.coreOutDataBitWidth * config.coreOutChannelCount bits))
+
+  def connect[T1 <: Data, T2 <: Data](from: Stream[T1], to: Stream[T2]) = {
+    to.arbitrationFrom(from)
+    to.payload.assignFromBits(from.payload.asBits)
+  }
+  def connect[T1 <: Data, T2 <: Data](from: Stream[T1], to: Flow[T2]) = {
+    to.valid := from.valid
+    from.ready := True
+    to.payload.assignFromBits(from.payload.asBits)
+  }
+  connect(din_stream, core.din)
+  connect(kernel_data_stream, core.kernel_data)
+  connect(requantizer_param_stream, core.requantizer_param_in)
+  connect(bias_data, core.bias_data)
+  connect(core.dout, dout)
+  core.line_width_sel := line_width_sel
 }
