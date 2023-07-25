0e00a65acb0b77154597894505dbe8d641315744
H1MSK <ksda47832338@outlook.com>
2023-07-25 12:27:16 +0800

    fix: port bit width on condition that scale is not enabled

--- hw/spinal/lib/quantizer/RequantizerParamBundle.scala
+++ hw/spinal/lib/quantizer/RequantizerParamBundle.scala
@@ -7,7 +7,7 @@ import scala.language.postfixOps
 case class RequantizerParamBundle(
     config: RequantizerConfig
 ) extends Bundle {
-  val scale = SInt(config.scale_bitwidth bits)
-  val offset = config.useOffset generate SInt(config.din_bitwidth + config.scale_bitwidth bits)
-  val shift_count = config.useRightShift generate UInt(log2Up(config.dout_bitwidth) bits)
+  val scale = (config.scale_bitwidth > 0) generate SInt(config.scale_bitwidth bits)
+  val offset = config.useOffset generate SInt(config.din_bitwidth + Math.max(config.scale_bitwidth, 0) bits)
+  val shift_count = config.useRightShift generate UInt(log2Up(config.din_bitwidth + Math.max(config.scale_bitwidth, 0)) bits)
 }
