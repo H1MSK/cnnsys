d22bf528fc2450dab9763796fc0d5c3e97a26b77
H1MSK <ksda47832338@outlook.com>
2023-07-29 23:13:43 +0800

    add: TestFragmentRecorder & TestInput2DPadder

--- hw/spinal/test/lib/TestAll.scala
+++ hw/spinal/test/lib/TestAll.scala
@@ -7,5 +7,7 @@ object TestAll extends TestTaskGenerator {
   def prepare(included: Boolean): Unit = {
     StreamController.TestAll.prepare(included)
     TestWindowedHistory2D.prepare(included)
+    TestFragmentRecorder.prepare(included)
+    TestInput2DPadder.prepare(included)
   }
 }
