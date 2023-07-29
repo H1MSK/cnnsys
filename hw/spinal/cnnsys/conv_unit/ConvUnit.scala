48dd75966ec450193fd15fa1fa7392e96fef6a67
H1MSK <ksda47832338@outlook.com>
2023-07-29 23:05:53 +0800

    chore: local function renaming & core/unit distinguish

--- hw/spinal/cnnsys/conv_unit/ConvUnit.scala
+++ hw/spinal/cnnsys/conv_unit/ConvUnit.scala
@@ -14,12 +14,12 @@ case class ConvUnit(config: ConvUnitConfig) extends Component {
     Axi4StreamConfig((bitCount.value + 7) / 8, useLast = useLast)
   )
 
-  def connect[T1 <: Data, T2 <: Data](from: Stream[T1], to: Stream[T2]) = {
+  def connectStreams[T1 <: Data, T2 <: Data](from: Stream[T1], to: Stream[T2]) = {
     to.arbitrationFrom(from)
     to.payload.assignFromBits(from.payload.asBits)
   }
 
-  def connect[T1 <: Data, T2 <: Data](from: Stream[T1], to: Flow[T2]) = {
+  def connectStreamToFlow[T1 <: Data, T2 <: Data](from: Stream[T1], to: Flow[T2]) = {
     to.valid := from.valid
     from.ready := True
     to.payload.assignFromBits(from.payload.asBits)
@@ -28,10 +28,10 @@ case class ConvUnit(config: ConvUnitConfig) extends Component {
   val line_width_sel = in Bits (log2Up(config.supportedInputWidths.length) bits)
 
   val din_stream = slave(genStreamPort(config.unitInDataBitWidth * config.coreInChannelCount * config.coreCount bits, useLast = true))
-  val kernel_data_stream = slave(genStreamPort(config.coreKernelDataBitWidth * config.coreInChannelCount * config.coreCount bits))
+  val kernel_data_stream = slave(genStreamPort(config.unitKernelDataBitWidth * config.coreInChannelCount * config.coreCount bits))
   val requantizer_param_stream = slave(genStreamPort(RequantizerParamBundle(config.requantizer_config).getBitsWidth * config.coreCount bits))
   val bias_data = slave(genStreamPort(config.biasDataBitWidth * config.coreOutChannelCount * config.coreCount bits))
-  val dout = master(genStreamPort(config.coreOutDataBitWidth * config.coreOutChannelCount * config.coreCount bits, useLast = true))
+  val dout = master(genStreamPort(config.unitOutDataBitWidth * config.coreOutChannelCount * config.coreCount bits, useLast = true))
 
   val padding_data = in SInt (config.coreInDataBitWidth bits)
   val padding_size = in UInt (log2Up(config.maxPaddingSize + 1) bits)
@@ -56,7 +56,7 @@ case class ConvUnit(config: ConvUnitConfig) extends Component {
 
   recorder.din_frags << padder.dout
 
-  connect(recorder.din_stream, core.din)
+  connectStreams(recorder.din_stream, core.din)
 
   recorder.dout_stream.arbitrationFrom(core.dout)
   recorder.dout_stream.payload.assignFromBits(core.dout.payload.asBits)
@@ -65,9 +65,9 @@ case class ConvUnit(config: ConvUnitConfig) extends Component {
   dout.payload.data.assignFromBits(recorder.dout_frags.fragment.asBits)
   dout.payload.last := recorder.dout_frags.last
 
-  connect(kernel_data_stream, core.kernel_data)
-  connect(requantizer_param_stream, core.requantizer_param_in)
-  connect(bias_data, core.bias_data)
+  connectStreamToFlow(kernel_data_stream, core.kernel_data)
+  connectStreamToFlow(requantizer_param_stream, core.requantizer_param_in)
+  connectStreamToFlow(bias_data, core.bias_data)
   core.line_width_sel := line_width_sel
 
   XilinxBusTagger.tag(din_stream, "din")
