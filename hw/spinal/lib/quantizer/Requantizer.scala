b70909bc96c3b1be9dee3b1b2b1dd707c30e0543
H1MSK <ksda47832338@outlook.com>
2023-07-29 23:08:34 +0800

    refactor: roundToInf

--- hw/spinal/lib/quantizer/Requantizer.scala
+++ hw/spinal/lib/quantizer/Requantizer.scala
@@ -59,7 +59,20 @@ case class Requantizer(
     val rounded =
       (if (delta > 0)
          Vec(
-           last_payload.map(_.roundToInf(last_payload.head.getBitsWidth - config.dout_bitwidth bits, align = false).sat(1))
+           last_payload.zipWithIndex.map(t => {
+             val x = t._1
+             val i = t._2
+             val x_shifted = x(delta, config.dout_bitwidth bits).setName("round_shifted_" + i)
+             val x_negative = x.msb.setName("round_negative_" + i)
+
+             val x_num_part = x_shifted(0, config.dout_bitwidth - 2 bits).setName("round_num_part_" + i)
+             val x_saturated = Mux(x_negative, x_num_part.orR === False, x_num_part.andR === True).setName("round_saturated_" + i)
+
+             val x_carry1 = x(delta - 1).setName("round_carry1_" + i)
+             val x_rest_any = (if (delta >= 2) x(0, delta - 1 bits).orR else False).setName("round_rest_any_" + i)
+             val x_delta = Mux(!x_saturated && x_carry1 && ((!x_negative) || x_rest_any), S(1), S(0)).setName("round_delta_" + i)
+             x_shifted + x_delta
+           })
          )
        else
          Vec(
