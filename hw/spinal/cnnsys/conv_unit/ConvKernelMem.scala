294fb91ff21908d0778e8ebdbf08d169b63a3ae2
H1MSK <ksda47832338@outlook.com>
2023-07-30 11:23:21 +0800

    fix: Register condition of last reg in ConvKernelMem

--- hw/spinal/cnnsys/conv_unit/ConvKernelMem.scala
+++ hw/spinal/cnnsys/conv_unit/ConvKernelMem.scala
@@ -9,7 +9,7 @@ case class ConvKernelMem(mem_size: Int, data_width: Int) extends Component {
   val din = slave Flow SInt(data_width bits)
   val dout = master Flow SInt(data_width bits)
 
-  val regs = Vec(History(RegNext(din.payload, din.payload.getZero), mem_size, din.valid, din.payload.getZero).reverse)
+  val regs: Vec[SInt] = Vec(History(RegNextWhen(din.payload, din.valid, init = din.payload.getZero), mem_size, din.valid, din.payload.getZero).reverse)
   regs.indices.foreach(i => {
     val r = regs(i)
     out(r)
